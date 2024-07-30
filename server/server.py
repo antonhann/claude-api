e8320f8e81a90bae4cb71e3120d6c798610bc45b
antonhann <antonha016@gmail.com>
2024-07-30 12:46:12 -0400

    invalidated server just for looks now

--- server/server.py
+++ server/server.py
@@ -1,61 +1,60 @@
-from flask import Flask, request, jsonify
-from pymongo import MongoClient
-import os
-from dotenv import load_dotenv
-from anthropic import Anthropic
+# from flask import Flask, request, jsonify
+# from pymongo import MongoClient
+# import os
+# from dotenv import load_dotenv
+# from anthropic import Anthropic
 
-app = Flask(__name__)
-mongoClient = MongoClient("")
-client = Anthropic()
-db = mongoClient["stemkasa"]
+# app = Flask(__name__)
+# client = Anthropic()
+# db = mongoClient["stemkasa"]
 
-"""
-API CALLS NEEDED:
+# """
+# API CALLS NEEDED:
 
-SEND MESSAGE
-CHAT HISTORY
-MAKING THE BOT
+# SEND MESSAGE
+# CHAT HISTORY
+# MAKING THE BOT
 
-"""
+# """
 
-"""
-documentation
-"""
-@app.route('/api/send-message', methods = ["POST"])
-def sendMessage():
-    return {"success" : True}
-
-
-"""
-Retrieve user's chat history with bot
-
-@params: 
-"""
-@app.route('/api/get-chat-history/<string:userId>/<int:botId>', methods=['GET'])
-def getChatHistory(userId,botId):
-        chats = db["STE_Chats"]
-        mongoHistory = chats.find({
-            "FROM_USER_ID": userId,
-            "BOT_ID": botId
-        })
-        history = [serializeDocument(document) for document in mongoHistory]
-        if history:
-            return jsonify({"history" : history})
-        else:
-            return jsonify({"message": "couldnt find message"}), 400
+# """
+# documentation
+# """
+# @app.route('/api/send-message', methods = ["POST"])
+# def sendMessage():
+#     return {"success" : True}
 
 
-def serializeDocument(doc):
-    """
-    Convert MongoDB document to a JSON serializable format.
-    """
-    if '_id' in doc:
-        doc['_id'] = str(doc['_id'])  # Convert ObjectId to string
-    if 'CREATION_DATE' in doc:
-        doc['CREATION_DATE'] = doc['CREATION_DATE'].isoformat()  # Convert datetime to ISO format
-    if 'LAST_UPDATE_DATE' in doc:
-        doc['LAST_UPDATE_DATE'] = doc['LAST_UPDATE_DATE'].isoformat()  # Convert datetime to ISO format
-    return doc
+# """
+# Retrieve user's chat history with bot
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# @params: 
+# """
+# @app.route('/api/get-chat-history/<string:userId>/<int:botId>', methods=['GET'])
+# def getChatHistory(userId,botId):
+#         chats = db["STE_Chats"]
+#         mongoHistory = chats.find({
+#             "FROM_USER_ID": userId,
+#             "BOT_ID": botId
+#         })
+#         history = [serializeDocument(document) for document in mongoHistory]
+#         if history:
+#             return jsonify({"history" : history})
+#         else:
+#             return jsonify({"message": "couldnt find message"}), 400
+
+
+# def serializeDocument(doc):
+#     """
+#     Convert MongoDB document to a JSON serializable format.
+#     """
+#     if '_id' in doc:
+#         doc['_id'] = str(doc['_id'])  # Convert ObjectId to string
+#     if 'CREATION_DATE' in doc:
+#         doc['CREATION_DATE'] = doc['CREATION_DATE'].isoformat()  # Convert datetime to ISO format
+#     if 'LAST_UPDATE_DATE' in doc:
+#         doc['LAST_UPDATE_DATE'] = doc['LAST_UPDATE_DATE'].isoformat()  # Convert datetime to ISO format
+#     return doc
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
